77440cb1a99839ac9ab877b88f4b82caa1405d4d
Naomi <naomi.janet05@gmail.com>
2025-03-29 19:33:26 +0800

    idk whats wrong

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
       vsync: this,
     );
     _controller.forward();
-    
+
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacementNamed(context, '/welcome');
     });
